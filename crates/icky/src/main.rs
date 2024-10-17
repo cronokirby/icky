30ddf97b19cc1b7c1107b1c19752ebbb81c008ac
Lucas Meier <lucas@cronokirby.com>
2024-10-17 00:12:53 -0700

    Make CLI evaluate the file passed into it

--- crates/icky/src/main.rs
+++ crates/icky/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Woof Woof!");
+use std::{env, fs, io, process::exit};
+
+use icky::eval;
+
+fn main() -> io::Result<()> {
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("expected usage:\n  icky <FILE>");
+        exit(1);
+    }
+    let source = fs::read_to_string(&args[1])?;
+    println!("{:?}", eval(&source));
+    Ok(())
 }
